3205bb1864d773a9be709170ab5f785cf5ae1a0b
Zheng Li <jk_zhengli@hotmail.com>
2016-07-18 12:42:06 -0400

    app now correctly modifies prices after order, just need to do so if the customer purchased multiple products

--- test.rb
+++ test.rb
@@ -2,8 +2,7 @@ require 'sinatra'
 require 'openssl'
 require 'httparty'
 require 'json'
-require 'base64'
-require 'rest_client'
+#require 'base64'
 require 'shopify_api'
 
 SHARED_SECRET = 'a2e5e6d5a356c6aa2f3db29046cd63cdd71ac02f8d0ff30fdbf567db575cbdde'
@@ -11,6 +10,7 @@ API_KEY = '3807ea8d65c3831a591d35dbf1d2bb59'
 PASSWORD = '068699831be90deba1024bfc1242ab49'
 STORE_NAME = 'test-store-1737.myshopify.com'
 url = "https://#{API_KEY}:#{PASSWORD}@#{STORE_NAME}/admin"
+ShopifyAPI::Base.site = url
 
 # helpers do
 #   def verify_webhook(data, hmac_header)
@@ -25,9 +25,8 @@ post '/order' do
 	data = JSON.parse request.body.read
 	#verified = verify_webhook(data, env["HTTP_X_SHOPIFY_HMAC_SHA256"])
 	item_count = data['line_items'].count
-	a = data['line_items'][0]['id']
+	a = data['line_items'][0]['product_id']
 	product = ShopifyAPI::Product.find(a)
-	a['price'] = 5.00
-	#RestClient.put "#{url}/products/", jdata, {:content_type => :json}
-	PUT "#{url}/admin/products/7509869767.json
+	product.variants[0].price = product.variants[0].price.to_f + 1
+	product.save
 end
